ba304024f4f59c076cd45422683dcaa4157af942
ooligors <92822738+ooligors@users.noreply.github.com>
2022-06-06 12:12:10 +0000

    Deploying to gh-pages from @ ooligors/goit-js-hw-09@40ea3bbdcc24cbea76d0f0ae49bec3922acabe43 🚀

---
+++ 01-color-switcher.0c1b935a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");function o(){n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}let r;t.addEventListener("click",(function(){t.disabled=!0,r=setInterval(o,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(r)}));
+//# sourceMappingURL=01-color-switcher.0c1b935a.js.map
---
+++ 01-color-switcher.537dd754.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),o=document.querySelector("body");function a(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}e.addEventListener("click",(function(){e.disabled=!0,t=setInterval(a,1e3)})),n.addEventListener("click",(function(){e.disabled=!1,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.537dd754.js.map
